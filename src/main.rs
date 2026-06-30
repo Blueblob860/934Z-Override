9f13f42688750ac068b5f1ddebe65b25b0dacd07
Blueblob860 <50087969+Blueblob860@users.noreply.github.com>
2026-06-30 19:35:27 -0400

    feat: :tada: Template Code

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use vexide::prelude::*;
+
+struct Robot {}
+
+impl Compete for Robot {
+    async fn autonomous(&mut self) {
+        println!("Autonomous!");
+    }
+
+    async fn driver(&mut self) {
+        println!("Driver!");
+    }
+}
+
+#[vexide::main]
+async fn main(peripherals: Peripherals) {
+    let robot = Robot {};
+
+    robot.compete().await;
+}
